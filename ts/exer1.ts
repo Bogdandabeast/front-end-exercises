74d0a4b752df507e8610d818a620478b905f89f7
Bogdandabeast <bogdan.console@gmail.com>
2025-10-08 19:08:02 +0200

    feat(ejercicios completados) : ejercicios terminados

---
+++ ts/exer1.ts
@@ -0,0 +1,33 @@
+
+interface Animal {
+    name: string;
+    canEat: boolean;
+    canDrink: boolean;
+    canSleep: boolean;
+}
+
+interface Bird extends Animal {
+    canFly: boolean;
+}
+
+interface Dog extends Animal {
+    race: string;
+    age: number;
+}
+
+const bird: Bird = {
+    name: 'Birdy',
+    canEat: true,
+    canDrink: true,
+    canSleep: true,
+    canFly: true
+};
+
+const dog: Dog = {
+    name: 'Dogy',
+    canEat: true,
+    canDrink: true,
+    canSleep: true,
+    race: 'some-race',
+    age: 1
+};
---
+++ ts/test.ts
@@ -0,0 +1,8 @@
+const funcionTipada = (id : string) : {id: string, name : string}=> {
+    return {
+        id: id,
+        name: "Bogdan"
+    }
+}
+
+console.table(funcionTipada("123"));
