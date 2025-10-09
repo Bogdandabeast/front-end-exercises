2e9f651c9c700e0de8d34f0a7777630ad11c5b15
Bogdan <bogdan.joboffers@gmail.com>
2025-10-09 12:22:07 +0200

    chore(prop component)

---
+++ vue/nuxt-vue-exercises/app/components/TheTitle.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+    const prop = defineProps<{
+        Title : string
+    }>()
+</script>
+
+<template>
+    <div>
+        <h3>El titulo del prop: {{ prop.Title }}</h3>
+    </div>
+</template>
